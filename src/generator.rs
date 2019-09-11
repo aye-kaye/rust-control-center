8febd0f8f3098fa732ad4c609fabde499b066451
Aleksey Kondrashkov <aeteros@googlemail.com>
2019-09-12 00:41:15 +0300

    #1 CLI: Создание cfg-файла

--- src/generator.rs
+++ src/generator.rs
@@ -1,10 +1,30 @@
+extern crate chrono;
 extern crate rayon;
+
+use std::collections::HashSet;
+use std::error::Error;
+use std::fs::File;
+use std::io::ErrorKind;
+use std::option::Iter;
+use std::time::Instant;
 
 use rayon::prelude::*;
 
-use std::collections::HashSet;
+use self::chrono::{DateTime, Local};
 
 pub fn gen_cfg(warehouse_id_list: Vec<u32>, terminal_count: u32) -> () {
-    let wh_id_set: Vec<u32> = warehouse_id_list.clone().drain(..).collect::<Vec<u32>>();
-    println!("{}", wh_id_set.par_iter().sum::<u32>());
+    let wh_id_set: HashSet<_> = warehouse_id_list.clone().drain(..).collect();
+    let now: DateTime<Local> = Local::now();
+    let ts = now.format("%Y%m%d_%H%M%S");
+    wh_id_set.par_iter().for_each(|w| {
+        File::create(format!("W{}_T{}_{}.cfg", w, 1, &ts)).unwrap_or_else(|error| {
+            if error.kind() == ErrorKind::AlreadyExists {
+                File::create("tmp.cfg").unwrap_or_else(|error| {
+                    panic!("Error creating substitute file {:?}", error);
+                })
+            } else {
+                panic!("Error creating configuration file {:?}", error);
+            }
+        });
+    });
 }
