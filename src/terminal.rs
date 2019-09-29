e60bc18d881dc849e451abaef06d627def297a50
Aleksey Kondrashkov <aeteros@googlemail.com>
2019-09-30 00:46:30 +0300

    #1 CLI:  * сбор статистики (tx_rt, max, p90, avg, tx_count)  * генерация данных

--- src/terminal.rs
+++ src/terminal.rs
@@ -4,7 +4,7 @@ use serde::{Deserialize, Serialize};
 #[derive(Serialize, Deserialize, Debug)]
 pub struct TermLogRecord {
     pub time_started: u64,
-    #[serde(rename(serialize = "type"))]
+    #[serde(rename(serialize = "type", deserialize = "type"))]
     pub typ: TransactionType,
     pub running_time: u32,
     pub tx_running_time: u32,
